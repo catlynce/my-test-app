48aeee9702e87f88ac2f3d96d757f72cbcca2511
Nuno Martins <nuno@alvesmartins.eu>
2020-05-26 11:50:16 +0100

    init commit

---
+++ src/components/AnswerList.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="answer-wrapper">
+    <div v-for="answer in data" :key="answer._id">
+      <div class="answer">
+        <p>{{answer.description}}</p>
+        <span class="user">{{answer.user}}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['data']
+}
+</script>
+
+<style scoped>
+  .answer-wrapper {
+    display: flex;
+    margin-left: 30px;
+    flex-direction: row;
+  }
+  .answer {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    margin-right: 10px;
+    margin-bottom: 10px;
+    padding: 0;
+    width: 300px;
+    height: 120px;
+    border: 1px solid black;
+    background-color: lightgray;
+    border-radius: 5px;
+  }
+  .answer p {
+    flex-grow: 1;
+    margin: 0;
+    padding: 12px;
+  }
+  .answer .user {
+    padding: 4px;
+    padding-right: 14px;
+    font-weight: 700;
+    text-align: right;
+    background-color:lavender;
+    border-radius: 5px;
+  }
+</style>
---
+++ src/components/QuestionList.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <div v-for="question in data" :key="question._id" class="question-wrapper">
+      <div class="question">
+        <p>{{question.title}}</p>
+        <span class="user">{{question.user}}</span>
+      </div>
+
+      <answer-list :data="question.answers"></answer-list>
+    </div>
+  </div>
+</template>
+
+<script>
+import AnswerList from './AnswerList.vue'
+
+export default {
+  props: {
+    data: {
+      required: true
+    }
+  },
+  components: {
+    AnswerList
+  }
+}
+</script>
+
+<style scoped>
+  .question-wrapper {
+    display: flex;
+
+  }
+  .question {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    margin-bottom: 10px;
+    padding: 0;
+    width: 300px;
+    height: 120px;
+    border: 1px solid black;
+    background-color: cornflowerblue;
+    border-radius: 5px;
+  }
+  .question p {
+    flex-grow: 1;
+    margin: 0;
+    padding: 12px;
+  }
+  .question .user {
+    padding: 4px;
+    padding-right: 14px;
+    font-weight: 700;
+    text-align: right;
+    background-color:lavender;
+    border-radius: 5px;
+  }
+</style>
